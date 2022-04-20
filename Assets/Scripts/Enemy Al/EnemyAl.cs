ccaa0dcc75795af029eb6a63a4edb22a764d4cc3
viet2799 <nguyenducviet@gmail.com>
2022-04-21 02:47:50 +0700

    enemy ani

--- Assets/Scripts/Enemy Al/EnemyAl.cs
+++ Assets/Scripts/Enemy Al/EnemyAl.cs
@@ -22,23 +22,20 @@ public class EnemyAl : MonoBehaviour
 
     void Init()
     {
-        //Make box collider trigger
         GetComponent<BoxCollider2D>().isTrigger = true;
 
-        //Create Root object
         GameObject root = new GameObject(name + "_Root");
-        //Reset Position of Root to enemy object
+
         root.transform.position = transform.position;
-        //Set enemy object as child of root
+
         transform.SetParent(root.transform);
-        //Create Waypoints object
+
         GameObject waypoints = new GameObject("Waypoints");
-        //Reset waypoints position to root        
-        //Make waypoints object child of root
+
         waypoints.transform.SetParent(root.transform);
         waypoints.transform.position = root.transform.position;
-        //Create two points (gameobject) and reset their position to waypoints objects
-        //Make the points children of waypoint object
+        
+
         GameObject p1 = new GameObject("Point1");
         p1.transform.SetParent(waypoints.transform);
         p1.transform.position = root.transform.position;
@@ -47,7 +44,6 @@ public class EnemyAl : MonoBehaviour
         p2.transform.SetParent(waypoints.transform);
         p2.transform.position = root.transform.position;
 
-        //Init points list then add the points to it
         points = new List<Transform>();
         points.Add(p1.transform);
         points.Add(p2.transform);
@@ -60,25 +56,18 @@ public class EnemyAl : MonoBehaviour
 
     void MoveToNextPoint()
     {
-        //Get the next Point transform
         Transform goalPoint = points[nextID];
-        //Flip the enemy transform to look into the point's direction
         if (goalPoint.transform.position.x > transform.position.x)
             transform.localScale = new Vector3(-1, 1, 1);
         else
             transform.localScale = new Vector3(1, 1, 1);
-        //Move the enemy towards the goal point
         transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
-        //Check the distance between enemy and goal point to trigger next point
         if (Vector2.Distance(transform.position, goalPoint.position) < 0.2f)
         {
-            //Check if we are at the end of the line (make the change -1)
             if (nextID == points.Count - 1)
                 idChangeValue = -1;
-            //Check if we are at the start of the line (make the change +1)
             if (nextID == 0)
                 idChangeValue = 1;
-            //Apply the change on the nextID
             nextID += idChangeValue;
         }
     }
